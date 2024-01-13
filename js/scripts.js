eebbc6629bb4619555b8b4b76fb6cbf44be4f5c2
ak1937 <155441512+ak1937@users.noreply.github.com>
2024-01-13 16:04:38 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+/* owl js*/
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  autoplay:true,
+  autoplayTimeout:1500,
+  stagePadding:20,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
